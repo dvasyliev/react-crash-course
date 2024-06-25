f97f6779ce88995a5a64abc68fdea84c19845026
dvasyliev <dvasyliev.dev@gmail.com>
2024-06-25 14:27:28 +0200

    S03. L01. Your First Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
+function ProductCard() {
+  return (
+    <article>
+      <h2>iPhone 15 Pro</h2>
+      <img
+        src="images/iphone.png"
+        alt="iPhone 15 Pro"
+        width="128px"
+        height="128px"
+      />
+      <p>Specification:</p>
+      <ul>
+        <li>A17 Pro chip with 6-core GPU</li>
+        <li>3x or 5x Telephoto camera</li>
+        <li>Up to 29 hours video playback</li>
+      </ul>
+      <button>Buy (From $999)</button>
+    </article>
+  );
+}
+
 function App() {
-  return <div className="App">React Crash Course</div>;
+  return (
+    <div className="App">
+      <ProductCard />
+    </div>
+  );
 }
 
 export default App;
